8235d1a84f663a620a512d65bae0d4574398abb9
kerwin10010 <kerwin10010@gmail.com>
2019-07-10 16:51:34 +1200

    changes for catagory whitelisting

--- SentimentAnalysis.py
+++ SentimentAnalysis.py
@@ -1,5 +1,6 @@
 import csv
 import ibm_watson
+from TaxonomySearcher import TaxonomySearcher
 
 from ibm_watson import NaturalLanguageUnderstandingV1
 from ibm_watson.natural_language_understanding_v1 import Features, EntitiesOptions, KeywordsOptions, CategoriesOptions, SentimentOptions
@@ -10,59 +11,81 @@ service = NaturalLanguageUnderstandingV1(
     iam_apikey='3lu4pe1GCX_ey2JIq5fTh5MjccYo0peTs8PgUCOW6Jw8')
 
 
-file = open('user-id-targetTweet-PastTweets.csv', 'r', newline='')
-targetfile = open('user-id-target_entity+sentiment', 'a', newline='')
-featurefile = open('user-id-feature_entity+sentiment', 'a', newline='')
-targetwriter = csv.writer(targetfile)
+file = open('user-id-targetTweet-PastTweets.csv', 'r', newline='', encoding="utf-8")
+featurefile = open('user-id-feature_entity+sentiment.csv', 'a', newline='', encoding="utf-8")
 featurewriter = csv.writer(featurefile)
+searcher = TaxonomySearcher()
 
 reader = csv.reader(file)
 big_list = list(reader)
 
+
 for row in big_list:
 
-    twitter_screen_name = row[0];
+    twitter_screen_name = row[0]
     twitter_id = row[1]
     target_tweet = row[2]
     csvRow = [twitter_screen_name, twitter_id]
+    targets = []
 
     try:
         response = service.analyze(
             text=target_tweet,
             features=Features(sentiment=SentimentOptions(),
-                              keywords=KeywordsOptions()
+                              keywords=KeywordsOptions(),
+                              categories=CategoriesOptions()
                               )
         ).get_result()
 
-        for keyword in response['keywords']:
-            csvRow.append(keyword['text'] + ":" + str(response['sentiment']['document']['score']))
+        print(response)
+        csvRow.append(" ")
 
-        targetwriter.writerow(csvRow)
+        for target in response['keywords']:
+            if target['relevance'] > 0.5:
+                if searcher.searchTaxMap(target['text']):
+                    targets.append(target['text'])
 
-    except:
-        print("tweet malformed")
+    except Exception as e:
+        print("hi")
 
-    csvRow = [twitter_screen_name, twitter_id]
+    entityDict = {}
 
     for past_tweet in row[3:]:
 
-        response = service.analyze(
-            text=past_tweet,
-            features=Features(sentiment=SentimentOptions(), keywords=KeywordsOptions())
-        ).get_result()
+        try:
+            response = service.analyze(
+                text=past_tweet,
+                features=Features(sentiment=SentimentOptions(),
+                                  keywords=KeywordsOptions(), categories=CategoriesOptions() )
+            ).get_result()
 
-        print(past_tweet)
-        print(response)
+            for keyword in response['keywords']:
 
-        for keyword in response['keywords']:
-            csvRow.append(keyword['text'] + ":" + str(response['sentiment']['document']['score']))
+                if keyword['text'] in entityDict:
+                    entityDict[keyword['text']] += response['sentiment']['document']['score']
+                else:
+                    entityDict[keyword['text']] = response['sentiment']['document']['score']
 
+            # sentiment analysis here
 
-        # sentiment analysis here
+        except Exception as e:
+            print(e)
 
-    featurewriter.writerow(csvRow)
+    numFeatures = 5
 
-targetfile.close()
+    for x in range(numFeatures):
+
+        if len(entityDict) == 0:
+            break
+
+        mostLikedEntity = (max(entityDict, key=entityDict.get))
+        csvRow.append(mostLikedEntity)
+        entityDict.pop(mostLikedEntity)
+
+    for target in targets:
+        csvRow[2] = target
+        featurewriter.writerow(csvRow)
+
 featurefile.close()
 file.close()
 
---
+++ TaxonomySearcher.py
@@ -0,0 +1,28 @@
+import csv
+
+
+class TaxonomySearcher:
+
+    def __init__(self):
+
+        taxMap = {}
+        file = open('taxonomy-with-ids.en-US.csv', 'r', newline='', encoding="utf-8")
+        reader = csv.reader(file)
+        taxRows = list(reader)
+
+        for rows in taxRows:
+            for categories in rows[1:]:
+                taxMap[categories] = ""
+        self.taxList = list(taxMap.keys())
+
+    def searchTaxMap(self, term):
+        for item in self.taxList:
+            if term in item:
+                print(term + "- hit")
+                return True
+        else:
+            print(term + "- miss")
+            return False
+
+
+
